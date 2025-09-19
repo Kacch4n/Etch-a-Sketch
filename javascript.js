6df0cbcbab3f48528d4352089e9383f8f6ec87e8
Kacch4n <92391725+Kacch4n@users.noreply.github.com>
2025-09-18 20:09:53 -0500

    Establish basic grid structure keeping the same size

---
+++ javascript.js
@@ -0,0 +1,26 @@
+const squares = 16;
+const container = document.querySelector("#container");
+
+function createRows () {
+    for (i=0;i<squares;i++) {
+        const row = document.createElement("div");
+        row.classList.add("rows");
+        container.appendChild(row);
+    }
+};
+
+createRows();
+
+const rows = document.querySelectorAll(".rows");
+
+function createColumns () {
+    rows.forEach((row) => {
+        for (i=0;i<squares;i++) {
+            const column = document.createElement("div");
+            column.classList.add("columns");
+            row.appendChild(column);
+        };
+    });
+};
+
+createColumns();
