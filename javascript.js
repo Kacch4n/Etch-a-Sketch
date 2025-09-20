be0e66a5257d204f08af5b63953f6f5785911c12
Kacch4n <92391725+Kacch4n@users.noreply.github.com>
2025-09-19 22:05:44 -0500

    Add button and functions to change sketchpad size

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-const squares = 100;
+let numberRows = 20;
+let numberColumns = 10;
 const container = document.querySelector("#container");
 
 function createRows () {
-    for (i=0;i<squares;i++) {
+    for (i=0;i<numberRows;i++) {
         const row = document.createElement("div");
         row.classList.add("rows");
         container.appendChild(row);
@@ -11,11 +12,10 @@ function createRows () {
 
 createRows();
 
-const rows = document.querySelectorAll(".rows");
-
 function createColumns () {
+    let rows = document.querySelectorAll(".rows");
     rows.forEach((row) => {
-        for (i=0;i<squares;i++) {
+        for (i=0;i<numberColumns;i++) {
             const column = document.createElement("div");
             column.classList.add("columns");
             row.appendChild(column);
@@ -25,10 +25,32 @@ function createColumns () {
 
 createColumns();
 
-const columns = document.querySelectorAll(".columns");
-
-columns.forEach((column) => {
+function mouseover () {
+    let columns = document.querySelectorAll(".columns");
+    columns.forEach((column) => {
     column.addEventListener("mouseover", () => {
         column.style.background = "black";
     });
+});
+};
+
+mouseover();
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+    let sketchpadSizeRows = prompt("How many rows?");
+    let sketchpadSizeColumns = prompt("How many columns?");
+    if (sketchpadSizeRows == null || sketchpadSizeRows == "") {
+        text = "U cancelled the change";
+    } else {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.firstChild);
+        }
+        numberRows = sketchpadSizeRows;
+        createRows();
+        numberColumns = sketchpadSizeColumns;
+        createColumns();
+        mouseover();
+    }
 });
